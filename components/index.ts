d467712c2ea213f64542605d06e1da925f0386e7
Mohannad Saker <mhndsaker@gmail.com>
2023-10-07 23:03:34 +0300

    first commit

---
+++ components/index.ts
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import NavBar from "./Navbar";
+import Footer from "./Footer";
+import CarCard from "./CarCard";
+import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import ShowMore from "./ShowMore";
+import SearchBar from "./Searchbar";
+
+export {
+  Hero,
+  CarCard,
+  CustomButton,
+  CustomFilter,
+  Footer,
+  NavBar,
+  ShowMore,
+  SearchBar,
+};
